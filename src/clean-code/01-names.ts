119fe7f8ab0bbe21aa4cba9bcd14763d851efe93
David Cascos <davidcascosgimeno@gmail.com>
2023-06-07 23:31:56 +0200

    Rename variable names to make them more understandable

--- src/clean-code/01-names.ts
+++ src/clean-code/01-names.ts
@@ -2,19 +2,19 @@
 
     // Ejemplo
     // Archivos a evaluar - files to evaluate
-    const fs = [
-        { id: 1, f: false },
-        { id: 2, f: false },
-        { id: 3, f: true },
-        { id: 4, f: false },
-        { id: 5, f: false },
-        { id: 7, f: true },
-    ]; 
-    
-    // Archivos marcados para borrar - files to delete
-    const arhivos = fs.map( f => f.f );
+    const filesToEvaluate = [
+        { id: 1, canDelete: false },
+        { id: 2, canDelete: false },
+        { id: 3, canDelete: true },
+        { id: 4, canDelete: false },
+        { id: 5, canDelete: false },
+        { id: 7, canDelete: true },
+    ];
 
-    
+    // Archivos marcados para borrar - files to delete
+    const filesToDelete = filesToEvaluate.map(file => file.canDelete);
+
+
     class AbstractUser { };
     class UserMixin { };
     class UserImplementation { };
@@ -26,30 +26,26 @@
 
 
     // Todo: Tarea
-        
+
     // día de hoy - today
-    const ddmmyyyy = new Date();
-    
+    const today = new Date();
+
     // días transcurridos - elapsed time in days
-    const d: number = 23;
-    
+    const elapsedDays: number = 23;
+
     // número de archivos en un directorio - number of files in directory
-    const dir = 33;
-    
+    const filesInDirectory = 33;
+
     // primer nombre - first name
-    const nombre = 'Fernando';
-    
+    const firstName = 'Fernando';
+
     // primer apellido - last name
-    const apellido = 'Herrera';
+    const lastName = 'Herrera';
 
     // días desde la última modificación - days since modification
-    const dsm = 12;
-    
-    // cantidad máxima de clases por estudiante - max classes per student
-    const max = 6;
+    const daysSinceModification = 12;
 
+    // cantidad máxima de clases por estudiante - max classes per student
+    const maxClassesPerStudent = 6;
 
 })();
-
-
-
