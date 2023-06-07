1d329e89c5775f4ec01d8bf18d08cba5da7f621f
David Cascos <davidcascosgimeno@gmail.com>
2023-06-08 00:44:04 +0200

    Rename function names to make them more understandable

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,27 +1,33 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies(movieId: string) {
-        console.log({ movieId });
-    }
-
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors(id: string) {
+    function getMovieInfoById(id: string) {
         console.log({ id });
     }
 
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getActorsByMovieId(movieId: string) {
+        console.log({ movieId });
+    }
+
     // funcion para obtener el bio del actor por el id
-    function getUsuario(ActorId: string) {
-        console.log({ ActorId });
+    function getActorBioById(id: string) {
+        console.log({ id });
     }
 
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[]) {
+    interface Movie {
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+    function createMovie({ title, description, rating, cast }: Movie) {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists(fullName: string, birthdate: Date): boolean {
+    function createActor(fullName: string, birthdate: Date): boolean {
         // tarea asincrona para verificar nombre
         // ..
         // ..
